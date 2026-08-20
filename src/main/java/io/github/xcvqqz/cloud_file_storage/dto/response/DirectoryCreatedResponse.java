288d185ec6559c6d02fc40a155460d3538aa3131
xcvqqz <Grossholder77@gmail.com>
2026-08-20 08:33:14 +0300

    feat(dto): create DirectoryCreatedResponse, ResourceResponseDTO;

---
+++ src/main/java/io/github/xcvqqz/cloud_file_storage/dto/response/DirectoryCreatedResponse.java
@@ -0,0 +1,10 @@
+package io.github.xcvqqz.cloud_file_storage.dto.response;
+
+public record DirectoryCreatedResponse(
+
+        String path,
+        String name,
+        String type
+)
+
+{}
---
+++ src/main/java/io/github/xcvqqz/cloud_file_storage/dto/response/ResourceResponseDTO.java
@@ -0,0 +1,11 @@
+package io.github.xcvqqz.cloud_file_storage.dto.response;
+
+public record ResourceResponseDTO(
+
+        String path,
+        String name,
+        byte size,
+        String type
+
+)
+{}
